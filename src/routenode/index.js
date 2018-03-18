9c2b541faed1f512f15d335cd668f04cab970035
Julia Long <julia.long@duke.edu>
2018-03-17 20:03:56 -0700

    Add Node modules

---
+++ src/routenode/index.js
@@ -0,0 +1,10 @@
+ 
+ const express = require('express');
+ const bodyParser = require('body-parser');
+ const calculator = require('RouteCalculator');
+ const app = express();
+ app.use(bodyParser.json());
+
+app.post('/index', function(request, result) {
+        
+    })
